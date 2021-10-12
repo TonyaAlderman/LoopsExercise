1e1d3c6974c8e535a491699945c8805ab6e65c5f
TonyaAlderman <talderm99@gmail.com>
2021-10-11 19:56:04 -0500

    Finished LoopExercise

---
+++ script.js
@@ -0,0 +1,24 @@
+for (i = 1; i <= 7; i++){
+    console.log(i);
+}
+
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+}
+
+const wizards = [
+    "Harry Potter",
+    "Hermoine Granger",
+    "Ron Weasley",
+]
+
+for (muggle of wizards) {
+    console.log(muggle);
+}
+
+let harryPotterMovies = 0
+while (harryPotterMovies < 8) {
+    harryPotterMovies++;
+}
+
+console.log(harryPotterMovies);
